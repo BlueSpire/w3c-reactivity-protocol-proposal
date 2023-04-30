a629a1282841d79ae8c4d03bf0d1b5b51f5dc76c
Rob Eisenberg <rob@bluespire.com>
2023-04-30 15:53:18 -0400

    chore: stub out test reactivity engines

---
+++ packages/test-reactivity-engine-two/src/index.ts
@@ -0,0 +1,13 @@
+import { Observer, ReactivityEngine } from "@bluespire/reactivity";
+
+export const testReactivityEngineTwo: ReactivityEngine = {
+  onAccess: function (target: object, propertyKey: string | symbol): void {
+    throw new Error("Function not implemented.");
+  },
+  onChange: function (target: object, propertyKey: string | symbol, oldValue: any, newValue: any): void {
+    throw new Error("Function not implemented.");
+  },
+  createFunctionObserver: function (func: Function): Observer {
+    throw new Error("Function not implemented.");
+  }
+};
